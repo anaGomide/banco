a1eb4511ca329f3b29a285a683aa2ba73cc5b690
Ana Gomide <analuizagomide.n@gmail.com>
2025-02-11 21:30:09 -0300

    Feat -Atualizar a estrutura principal do aplicativo com roteamento e inicialização da formatação de data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
+import 'views/card_screen.dart';
+import 'views/fatura_screen.dart';
 import 'views/home_screen.dart';
+import 'views/shop_screen.dart';
+import 'views/trasaction_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR', null);
   runApp(MyApp());
 }
 
@@ -19,28 +26,29 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp(
+          initialRoute: '/',
+          routes: {
+            HomeScreen.routeName: (context) => HomeScreen(),
+            FaturaPage.routeName: (context) => FaturaPage(),
+            CardPage.routeName: (context) => CardPage(),
+            ShopPage.routeName: (context) => ShopPage(),
+            UltimosLancamentosScreen.routeName: (context) => UltimosLancamentosScreen(transactions: []),
+          },
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             fontFamily: GoogleFonts.mulish().fontFamily,
             textTheme: TextTheme(
-              displayLarge: GoogleFonts.mulish(fontSize: 32.sp, fontWeight: FontWeight.bold, color: Colors.black),
-              displayMedium: GoogleFonts.mulish(fontSize: 28.sp, fontWeight: FontWeight.bold, color: Colors.black),
-              displaySmall: GoogleFonts.mulish(fontSize: 24.sp, fontWeight: FontWeight.bold, color: Colors.black),
-              headlineLarge: GoogleFonts.mulish(fontSize: 22.sp, fontWeight: FontWeight.w700, color: Colors.black),
               headlineMedium: GoogleFonts.mulish(fontSize: 20.sp, fontWeight: FontWeight.w600, color: Colors.black),
-              headlineSmall: GoogleFonts.mulish(fontSize: 18.sp, fontWeight: FontWeight.w500, color: Colors.black),
+              headlineSmall: GoogleFonts.mulish(fontSize: 20.sp, fontWeight: FontWeight.w600, color: Colors.white),
               titleLarge: GoogleFonts.mulish(fontSize: 16.sp, fontWeight: FontWeight.w600, color: Colors.black),
               titleMedium: GoogleFonts.mulish(fontSize: 14.sp, fontWeight: FontWeight.w500, color: Colors.black54),
-              titleSmall: GoogleFonts.mulish(fontSize: 12.sp, fontWeight: FontWeight.w400, color: Colors.black54),
               bodyLarge: GoogleFonts.mulish(fontSize: 16.sp, fontWeight: FontWeight.normal, color: Colors.black),
-              bodyMedium: GoogleFonts.mulish(fontSize: 14.sp, fontWeight: FontWeight.normal, color: Colors.black54),
-              bodySmall: GoogleFonts.mulish(fontSize: 12.sp, fontWeight: FontWeight.normal, color: Colors.black54),
+              bodySmall: GoogleFonts.mulish(fontSize: 10.sp, fontWeight: FontWeight.w600, color: const Color.fromARGB(255, 0, 0, 0)),
               labelLarge: GoogleFonts.mulish(fontSize: 16.sp, fontWeight: FontWeight.w600, color: Colors.black),
-              labelMedium: GoogleFonts.mulish(fontSize: 14.sp, fontWeight: FontWeight.w500, color: Colors.black),
-              labelSmall: GoogleFonts.mulish(fontSize: 12.sp, fontWeight: FontWeight.w400, color: Colors.black),
+              labelMedium: GoogleFonts.mulish(fontSize: 14.sp, fontWeight: FontWeight.w600, color: Colors.black),
+              labelSmall: GoogleFonts.mulish(fontSize: 8.sp, fontWeight: FontWeight.w400, color: Colors.black),
             ),
           ),
-          home: HomeScreen(),
         );
       },
     );
